ea1b3dfdaf199562b8695f3b36047974b3eda04e
your name <1642984594@qq.com>
2018-10-14 00:30:02 +0800

    eureka注册中心启动成功了

--- server/src/main/java/com/eureka/server/CenterApplication.java
+++ server/src/main/java/com/eureka/server/CenterApplication.java
@@ -1,15 +1,14 @@
 package com.eureka.server;
-import demo.Demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaServer
 public class CenterApplication {
 
     public static void main(String[] args) {
-        Demo d= new Demo();
         SpringApplication.run(CenterApplication.class,args);
     }
 }

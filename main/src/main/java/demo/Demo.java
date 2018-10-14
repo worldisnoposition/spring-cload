5911bdfee456a5dcda6ef8373bbb789fb47bc228
your name <1642984594@qq.com>
2018-10-15 00:31:08 +0800

    瞎配置一通，乱写的我都搞乱了，有时间的好好梳理学习下

--- main/src/main/java/demo/Demo.java
+++ main/src/main/java/demo/Demo.java
@@ -1,4 +1,12 @@
 package demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Demo {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo.class,args);
+    }
 }

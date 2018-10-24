5a4c48564764c71517e5eab9a7af6d2e72ccc9bc
your name <1642984594@qq.com>
2018-10-24 23:57:02 +0800

    自以为spring还可以，结果自己配起来，焦头烂额，全是报错，到现在还没搞通，只是注册了一个服务，但还未以程序的方式调用服务

--- client/src/main/java/com/eureka/client/UserApplication.java
+++ client/src/main/java/com/eureka/client/UserApplication.java
@@ -1,11 +1,14 @@
 package com.eureka.client;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
+@SpringBootConfiguration
 public class UserApplication {
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(UserApplication.class,args);

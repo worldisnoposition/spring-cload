9c4df31d00a0a17945230fe84303787b196604b4
your name <1642984594@qq.com>
2018-10-24 23:53:27 +0800

    自以为spring还挺熟悉，结果自己一配起来就是懵逼，一堆报错，焦头烂额，好不容易注册了一个服务。

--- client/src/main/java/com/eureka/client/HomeController.java
+++ client/src/main/java/com/eureka/client/HomeController.java
@@ -1,10 +1,12 @@
-package com;
+package com.eureka.client;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Component
 public class HomeController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
